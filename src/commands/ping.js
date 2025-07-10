e5b01deb04250256808374fa0999ac5cd84811e7
MrGamel <mrgamelist01@gmail.com>
2025-07-10 10:06:08 +0300

    Create ping.js

---
+++ src/commands/ping.js
@@ -0,0 +1,14 @@
+// src/commands/ping.js
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Botun tepki süresini ölçer'),
+    
+  async execute(interaction) {
+    const sent = await interaction.reply({ content: 'Pong?', fetchReply: true });
+    const latency = sent.createdTimestamp - interaction.createdTimestamp;
+    await interaction.editReply(`Pong! 🏓 Latency: ${latency}ms`);
+  },
+};
