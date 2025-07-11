0be433f107220a5e9a100dc2a9418cddfbdad7e5
MrGamel <mrgamelist01@gmail.com>
2025-07-11 17:04:15 +0300

    Update language.js

--- commands/language.js
+++ commands/language.js
@@ -1,28 +1,24 @@
 // commands/language.js
 import { SlashCommandBuilder } from 'discord.js';
 import { setUserLang } from '../utils/langStorage.js';
+import { languages } from '../utils/languages.js';
 
 export const data = new SlashCommandBuilder()
   .setName('language')
-  .setDescription('Kendi dil ayarını değiştir')
-  .addStringOption(option =>
-    option
-      .setName('lang')
-      .setDescription('Yeni dil seç')
-      .setRequired(true)
-      .addChoices(
-        { name: 'Türkçe', value: 'tr' },
-        { name: 'English', value: 'en' }
-      )
-  );
+  .setDescription('Change your language')
+  .addStringOption(opt => {
+    opt.setName('lang')
+       .setDescription('Select a language')
+       .setRequired(true);
+    for (const lang of languages) {
+      opt.addChoices({ name: lang.name, value: lang.value });
+    }
+    return opt;
+  });
 
 export async function execute(interaction, i18next) {
-  const selectedLang = interaction.options.getString('lang');
-  setUserLang(interaction.user.id, selectedLang);
-  await i18next.changeLanguage(selectedLang);
-
-  await interaction.reply({
-    content: i18next.t('lang_changed'),
-    ephemeral: true
-  });
+  const chosen = interaction.options.getString('lang');
+  setUserLang(interaction.user.id, chosen);
+  await i18next.changeLanguage(chosen);
+  await interaction.reply({ content: i18next.t('lang_changed'), ephemeral: true });
 }
