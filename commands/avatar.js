6083f5ca69fcba4a70d0a151e35445306208b9e2
MrGamel <mrgamelist01@gmail.com>
2025-07-11 17:05:01 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,18 +3,16 @@ import { SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
   .setName('avatar')
-  .setDescription('Kendi veya başka bir kullanıcının avatarını gösterir')
-  .addUserOption(option =>
-    option
-      .setName('user')
-      .setDescription('Hedef kullanıcı')
-      .setRequired(false)
+  .setDescription('Show your or another user’s avatar')
+  .addUserOption(opt =>
+    opt.setName('user')
+       .setDescription('Target user')
+       .setRequired(false)
   );
 
-export async function execute(interaction, i18next) {
+export async function execute(interaction) {
   const user = interaction.options.getUser('user') || interaction.user;
   await interaction.reply({
-    content: `${user.username}:\n${user.displayAvatarURL({ dynamic: true })}`,
-    ephemeral: false
+    content: `${user.tag}'s avatar:\n${user.displayAvatarURL({ dynamic: true, size: 1024 })}`
   });
 }
