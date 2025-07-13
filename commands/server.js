cdaaf6ca82906925d7414da83d201b2a5a9011f9
MrGamel <mrgamelist01@gmail.com>
2025-07-13 10:59:56 +0300

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,18 +1,10 @@
-// commands/server.js
-import { SlashCommandBuilder } from 'discord.js';
+const { SlashCommandBuilder } = require('discord.js');
 
-export const data = new SlashCommandBuilder()
-  .setName('server')
-  .setDescription('Show server information');
-
-export async function execute(interaction, i18next) {
-  const guild = interaction.guild;
-  const owner = await guild.fetchOwner();
-  const info = [
-    `${i18next.t('server_name')}: ${guild.name}`,
-    `${i18next.t('server_id')}: ${guild.id}`,
-    `${i18next.t('server_owner')}: ${owner.user.tag}`,
-    `${i18next.t('server_member_count')}: ${guild.memberCount}`
-  ];
-  await interaction.reply({ content: info.join('\n') });
-}
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('server')
+    .setDescription('Sunucu bilgilerini gösterir'),
+  async execute(interaction) {
+    await interaction.reply(`Sunucu adı: ${interaction.guild.name}\nÜye sayısı: ${interaction.guild.memberCount}`);
+  }
+};
